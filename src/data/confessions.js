c247e0d8fc56764e34e0b1d5e4826613877db1da
nhi <nhi@MacBook-Air-cua-nhi.local>
2026-03-29 23:39:49 +0700

    check all

--- src/data/confessions.js
+++ src/data/confessions.js
@@ -3,15 +3,10 @@ export const anniversaryMessages = [
     id: 1,
     message:
       `Cảm ơn chị Linh đã luôn quan tâm và chăm sóc em trong suốt thời gian qua, từ những ngày đầu em mới ra Huế. Nhờ có chị mà em mới có được chỗ ở ổn định và thoải mái như hiện tại. Với một người lần đầu ra sống xa nhà, ở một thành phố lạ, mọi thứ đều bỡ ngỡ — nhưng may mắn là em đã có chị đồng hành lúc đó.
-
     Trong suốt 4 tháng ở cùng nhau, em thật sự rất vui. Dù mình là đồng nghiệp, nhưng cũng là lần đầu em có cơ hội nói chuyện và tiếp xúc với chị nhiều như vậy. Ban đầu em cũng không biết phải làm sao khi ở chung với một người chưa quen, nhưng nhờ sự dễ thương và thoải mái của chị mà mọi thứ trở nên rất tự nhiên.
-
     Giờ thì mình không còn làm cùng nhau nữa, mỗi người có một hướng đi riêng. Em chúc chị sẽ sớm tìm được công việc phù hợp, môi trường tốt, chỗ ở ổn định và ngày càng thành công hơn trong công việc cũng như cuộc sống. Mong là mọi điều tốt đẹp sẽ đến với chị trong chặng đường mới này.
-
     Em thì chưa biết khi nào sẽ về hẳn Đà Nẵng, nhưng cuối tuần em vẫn hay về. Có dịp chị em mình hẹn nhau đi cà phê, ngồi kể nhau nghe những câu chuyện mới ở công ty của chị nha. Chị cứ rủ là em sẽ cố gắng sắp xếp thời gian đi liền.
-
-    Khi chị về Đà Nẵng ổn định hơn, mình làm một bữa thật vui để gặp lại nhau nhé. Hy vọng là dù không còn ở chung hay làm cùng, tụi mình vẫn giữ liên lạc và thỉnh thoảng gặp nhau như những người bạn thân quen.
-
+    Khi chị về Đà Nẵng ổn định hơn, mình làm một bữa thật vui để gặp lại nhau nhé. Hy vọng là dù không còn ở chung hay làm cùng, 2 chị em vẫn giữ liên lạc và thỉnh thoảng gặp nhau như những người bạn thân quen.
     Chúc chị luôn vui vẻ, nhiều năng lượng và gặp thật nhiều may mắn 💛
     `,
     name: "Ý Nhi"
